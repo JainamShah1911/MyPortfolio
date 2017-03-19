02e2e934ed37148aa704420e7227ef340de9cd5f
Jainam Shah <JainamShah1911@gmail.com>
2017-03-20 04:22:55 +0530

    Updated the address and the google map location

--- controllers/connectController.js
+++ controllers/connectController.js
@@ -2,11 +2,11 @@ angular.module('myPortfolio').controller('connectController', ['$scope', functio
     
     $scope.name = 'Jainam Shah'; 
     $scope.cellNo = '+1 732.614.6164';
-    $scope.address = '25 Senate Pl, ';
-    $scope.apt = 'Unit 408, ';
+    $scope.address = '78 Palisade Ave, ';
+    $scope.apt = 'Apt 3, ';
     $scope.city = 'Jersey City, NJ - 07306';
     $scope.email = 'JainamShah1911@gmail.com';
-    $scope.location = 'https://goo.gl/maps/N2MWZDhnBNq';
+    $scope.location = 'https://goo.gl/maps/PYnmBAMyDB92';
     $scope.mailTo = 'mailto:JainamShah1911@gmail.com?subject=Nice to meet you ';
     $scope.school = 'New Jersey Institute of Technology';
     $scope.degree = 'Masters in Computer Science';
